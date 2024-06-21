3a88c71b51717e3dabaf4ec14c83942409f3536c
heejung1 <163121438+heejung1@users.noreply.github.com>
2024-06-21 12:44:08 +0900

    Update ReviewSentimentKakao.py

--- ReviewSentimentKakao.py
+++ ReviewSentimentKakao.py
@@ -2,16 +2,15 @@ import requests
 import json
 import pandas as pd
 import sys
+import re
 
 df = pd.read_csv('kakao_review.csv', encoding='utf-8-sig')
 
-import re
-
-#전처리
+# 전처리
 def preprocess_text(text):
-     text = re.sub(r'[^가-힣 ]', '', str(text), re.UNICODE)
-     text = re.sub(r'\b더보기\b', '', str(text))
-     text = re.sub('\s+', ' ', text).strip()
+     text = re.sub(r'[^가-힣 ]', '', str(text), re.UNICODE)  # 한글 제외한 문자 제거
+     text = re.sub(r'\b더보기\b', '', str(text))  # 더보기 문자 제거
+     text = re.sub('\s+', ' ', text).strip() # 불필요한 공백 문자 제거
      return text
 
 
@@ -23,6 +22,7 @@ df = df.drop_duplicates(subset=['리뷰'], keep='first')
 
 review_text = df['리뷰']
 
+# 네이버 API 사용 시 필요한 키로 개인 발급 받아야 함
 client_id = "8z47hfo7sw"
 client_secret = "OFnp7HqkiGS0WyuxjF1Fli3LGKzdnnIVquIYhNb4"
 
